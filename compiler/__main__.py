340fc99b8a6cd9b46ccabe52858880ab626086f0
GrandMoff100 <minecraftcrusher100@gmail.com>
2022-03-27 19:52:47 +0000

    Added interactive mode

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -21,4 +21,28 @@ def cli_compile(file: io.TextIOWrapper, verbose: int, quiet: int):
     compiler.compile(file.read(), verbosity=4 + quiet - verbose)
 
 
+@cli.command(name="compile")
+@click.argument("file", type=click.File("r"), default="main.star")
+@click.option("-v", "--verbose", count=True, help="generic help message")
+@click.option("-q", "--quiet", count=True, help="generic help message")
+def cli_compile(file: io.TextIOWrapper, verbose: int, quiet: int):
+    """lol3"""
+    compiler.compile(file.read(), verbosity=4 + quiet - verbose)
+
+
+@cli.command(name="interactive")
+@click.option("-v", "--verbose", count=True, help="generic help message")
+@click.option("-q", "--quiet", count=True, help="generic help message")
+def cli_interactive(verbose: int, quiet: int):
+    while True:
+        code = ""
+        click.echo("------ Code Editor ------")
+        while True:
+            line = input(": ")
+            if line.strip() == "":
+                break
+            code += line + "\n"
+        click.echo(compiler.compile(code, verbosity=4 + quiet - verbose))
+
+
 cli()  # pylint: disable=no-value-for-parameter
